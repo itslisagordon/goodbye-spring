b47c9c57b45850dde352bd14192135b39ea44e4b
Lisa Gordon <lg.official94@gmail.com>
2020-10-06 20:51:02 -0500

    My first commit

---
+++ src/main/java/org/launchcode/goodbyespring/GoodbyeSpringApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.goodbyespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbyeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/goodbyespring/GoodbyeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.goodbyespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodbyeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
